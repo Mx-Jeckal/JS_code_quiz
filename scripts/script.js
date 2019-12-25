ee37eaff5e186fcbb14e10bf4c8b23239c08034a
Hazel Tuxhorn <mx.jeckal@gmail.com>
2019-12-25 15:28:33 -0600

    layout and buttons working

--- scripts/script.js
+++ scripts/script.js
@@ -23,14 +23,27 @@ navEl.appendChild(navBtns)
 quizDiv.setAttribute('class', "quizDiv col-sm-8 col-lg-6")
 bgImg.appendChild(quizDiv)
 
-navCard.setAttribute('class', 'card-body cardImg')
+navCard.setAttribute('class', 'card-body cardBtn')
 navBtns.appendChild(navCard)
-navCard.innerHTML = "hey this is a card"
+navCard.innerHTML = "JS&nbsp;Quiz"
 
 navCard1.setAttribute('class', 'card-body cardBtn')
 navBtns.appendChild(navCard1)
-navCard1.innerHTML = "Button"
+navCard1.innerHTML = "HiScores"
 
 navCard2.setAttribute('class', 'card-body cardBtn')
 navBtns.appendChild(navCard2)
 navCard2.innerHTML = "Button"
+
+
+
+
+navCard.addEventListener('click', () => {
+    window.location.href = "https//:www.google.com"
+    console.log('btn clicked')
+})
+
+var newGame = document.createElement('div')
+newGame.setAttribute('class', "cardBtn game card-body")
+newGame.innerHTML = "Start&nbsp;New&nbsp;Game"
+quizDiv.appendChild(newGame)
